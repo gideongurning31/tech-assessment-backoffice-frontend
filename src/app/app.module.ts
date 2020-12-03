30d047f4d6a146d944b1c390e52e0d9528fcb687
Gideon Gurning <gideon.gurning@gmail.com>
2020-12-03 14:33:24 +0700

    Dashboard (nav-page-footer) base layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  template: `
+  <footer>
+    <p>All Rights Reserved 2020 © Gideon Oktajadi Nehemia</p>
+  </footer>`,
+  styleUrls: ['footer.component.scss'],
+})
+export class FooterComponent {
+  constructor() {}
+}

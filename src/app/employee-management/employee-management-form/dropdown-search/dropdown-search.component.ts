84430492a130a521f0ac817725ba1742b176e447
Gideon Gurning <gideon.gurning@gmail.com>
2020-12-04 21:21:39 +0700

    Used select2 library (works)

--- src/app/employee-management/employee-management-form/dropdown-search/dropdown-search.component.ts
+++ src/app/employee-management/employee-management-form/dropdown-search/dropdown-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { LabelValue } from 'src/app/common/label-value.model';
+declare let $: any;
 
 @Component({
   selector: 'app-dropdown-search',
@@ -17,7 +18,7 @@ export class DropdownSearchComponent implements OnInit {
   @Input() options: Array<LabelValue>;
 
   ngOnInit(): void {
-    this.options = [{ label: null, value: null }, ...this.options];
+    $('.select2').select2();
   }
 
   filter() {}

047b9d4b11528e887f2a43f2555e23f4e3be9e32
Gideon Gurning <gideon.gurning@gmail.com>
2020-12-05 15:19:23 +0700

    Rules for filter form element

--- src/app/employee-management/filter/filter.component.ts
+++ src/app/employee-management/filter/filter.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LabelValue } from 'src/app/common/label-value.model';
 
 @Component({
   selector: 'app-filter',
@@ -8,19 +10,55 @@ import { Component, OnInit } from '@angular/core';
 export class FilterComponent implements OnInit {
   constructor() {}
 
-  options: Array<string>;
+  options: Array<LabelValue>;
+
+  filterForm: FormGroup;
 
   ngOnInit() {
-    this.options = [
-      'Select Field',
-      'Username',
-      'First Name',
-      'Last Name',
-      'Email',
-      'Birth Date',
-      'Basic Salary',
-      'Status',
-      'Group',
-    ];
+    this.options = FILTER_SELECT;
+    this.initFilterForm();
+  }
+
+  initFilterForm() {
+    this.filterForm = new FormGroup({
+      key1: new FormControl('', Validators.required),
+      value1: new FormControl(null, Validators.required),
+      key2: new FormControl(''),
+      value2: new FormControl(null),
+    });
+    this.filterForm.controls.value1.disable();
+    this.filterForm.controls.value2.disable();
+  }
+
+  onSelectKey(i: number) {
+    if (this.sanitize(this.filterForm.controls['key' + i].value)) {
+      this.filterForm.controls['value' + i].enable();
+    } else {
+      this.filterForm.controls['value' + i].disable();
+    }
+  }
+
+  applyFilter() {
+    console.log(this.filterForm.value);
+  }
+
+  private sanitize(value: any) {
+    if (value === '' || value === [] || value === {}) {
+      return null;
+    } else {
+      return value;
+    }
   }
 }
+
+const FILTER_SELECT = [
+  { label: 'Select Field', value: '' },
+  { label: 'Username', value: 'username' },
+  { label: 'First Name', value: 'firstName' },
+  { label: 'Last Name', value: 'lastName' },
+  { label: 'Email', value: 'Email' },
+  { label: 'Birth Date', value: 'birthDate' },
+  { label: 'Basic Salary', value: 'basicSalary' },
+  { label: 'Status', value: 'status' },
+  { label: 'Group', value: 'group' },
+];
